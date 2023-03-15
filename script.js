c085070bce3a1b5976b9c3dfb6f17401dd2c4559
Adrian Mititelu <mititeluadrian9@gmail.com>
2023-03-15 14:34:07 +0300

    Fix code

--- script.js
+++ script.js
@@ -43,13 +43,14 @@ soundBtn = document.querySelector(".sound");
 
 motivationQuoteBtn.addEventListener("click", function() {
     const randomIndex = Math.floor(Math.random() * quotesMotivation.length);
+    motivationQuote.innerHTML = quotesMotivation[randomIndex];
     const currentQuote = motivationQuote.innerHTML;
     const quoteIndex = quotesMotivation.indexOf(currentQuote);
     if (quoteIndex !== -1) {
         authorsName.innerHTML = nameAuthor[quoteIndex];
     }
-    motivationQuote.innerHTML = quotesMotivation[randomIndex];
 });
+
 
 
 soundBtn.addEventListener("click", ()=> {
